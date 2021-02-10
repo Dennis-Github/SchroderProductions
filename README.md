65daf43d4466cbec52d3d99ddf1231b821bd7a1a
Dennis-Github <110062@st-maartenscollege.nl>
2021-02-10 09:56:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SchroderProductions
+ 
---
+++ SchroderProductions/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SchroderProductions.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
