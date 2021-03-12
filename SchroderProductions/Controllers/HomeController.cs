a8b63bd4dccc0035c52c824326e8a10b706b9a03
Dennis-Github <110062@st-maartenscollege.nl>
2021-03-12 12:52:21 +0100

    Kopje test

--- SchroderProductions/Controllers/HomeController.cs
+++ SchroderProductions/Controllers/HomeController.cs
@@ -28,6 +28,10 @@ namespace SchroderProductions.Controllers
             return View();
         }
 
+        public IActionResult Test()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
