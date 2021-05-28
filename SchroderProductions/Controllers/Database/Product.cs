d8118b1e940b42ab32b6625eb4ba729acddeb277
Dennis-Github <110062@st-maartenscollege.nl>
2021-05-28 12:47:26 +0200

    Product.cs

---
+++ SchroderProductions/Controllers/Database/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchroderProductions.Controllers.Database
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Naam { get; set; }
+
+        public string Prijs { get; set; }
+
+        public int Beschikbaarheid { get; set; }
+    }
+}
