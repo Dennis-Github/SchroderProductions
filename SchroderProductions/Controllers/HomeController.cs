8b72b1a1f18b1e32608d48a9f7c4a4069c850983
noa van bunge <111687@st-maartenscollege.nl>
2021-06-18 12:44:55 +0200

    Begin contact pagina

--- SchroderProductions/Controllers/HomeController.cs
+++ SchroderProductions/Controllers/HomeController.cs
@@ -25,13 +25,13 @@ namespace SchroderProductions.Controllers
         public IActionResult Index()
         {
             // alle namen ophalen
-            var products = GetProducts();
+            var products = GetFestivals();
 
             // stop de namen in de html
             return View(products);
         }
 
-        public List<Festival> GetProducts()
+        public List<Festival> GetFestivals()
         {
             // stel in waar de database gevonden kan worden
             string connectionString = "Server=172.16.160.21;Port=3306;Database=110062;Uid=110062;Pwd=Dennisenleon!;";
@@ -74,7 +74,50 @@ namespace SchroderProductions.Controllers
             // return de lijst met namen
             return products;
         }
-       [Route("Privacy")]
+        public Festival GetFestival(string id)
+        {
+            // stel in waar de database gevonden kan worden
+            string connectionString = "Server=172.16.160.21;Port=3306;Database=110062;Uid=110062;Pwd=Dennisenleon!;";
+
+            // maak een lege lijst waar we de namen in gaan opslaan
+            List<Festival> products = new List<Festival>();
+
+            // verbinding maken met de database
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                // verbinding openen
+                conn.Open();
+
+                // SQL query die we willen uitvoeren
+                MySqlCommand cmd = new MySqlCommand($"select * from festival where id ={id}", conn);
+
+                // resultaat van de query lezen
+                using (var reader = cmd.ExecuteReader())
+                {
+                    // elke keer een regel (of eigenlijk: database rij) lezen
+                    while (reader.Read())
+                    {
+                        Festival p = new Festival
+                        {
+                            // selecteer de kolommen die je wil lezen. In dit geval kiezen we de kolom "naam"
+                            Id = Convert.ToInt32(reader["Id"]),
+                            Plaats = reader["Plaats"].ToString(),
+                            Naam = reader["Naam"].ToString(),
+                            Prijs = Convert.ToInt32(reader["Prijs"]),
+                            Afbeelding = reader["Afbeelding"].ToString(),
+                            Logo = reader["Logo"].ToString(),
+                        };
+
+                        // voeg de naam toe aan de lijst met namen
+                        products.Add(p);
+                    }
+                }
+            }
+
+            // return de lijst met namen
+            return products[0];
+        }
+        [Route("Privacy")]
         public IActionResult Privacy()
         {
             return View();
